aeb724245f6112c3cc437d30209682874154347c
jithendraganthakoru <jganthakoru419@gmail.com>
2025-07-26 20:02:33 +0530

    commit initial version

--- src/main/java/com/example/controller/SpringbootAppController.java
+++ src/main/java/com/example/controller/SpringbootAppController.java
@@ -1,15 +1,19 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpringbootAppController {
 	
+      
+	@Value("${environment}")
+	private String env;
+	
 	
 	@GetMapping("/getResponse")
 	public String getResponse() {
-		
-		return "Welcome to India.....";	
+		return "Welcome to India....."+env;	
 	}
 }
